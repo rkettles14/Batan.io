6dcd0892de6cef12eb1cd4a0f71ffd2104555bd1
rkettles14 <rkettles14@gmail.com>
2020-10-27 20:41:51 -0600

    initial commit

---
+++ enums.ts
@@ -0,0 +1,23 @@
+// enum vertexStatus {
+//     open,
+//     settlement,
+//     city,
+//     blocked
+// }
+
+// enum player {
+//     none,
+//     red,
+//     white,
+//     blue,
+//     orange
+// }
+
+// enum resourceType {
+//     none,
+//     sheep,
+//     wheat,
+//     wood,
+//     ore,
+//     brick
+// }
