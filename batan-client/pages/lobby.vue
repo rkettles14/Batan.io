20ad4e4883377aeed5c7770f1442f4455b001fe4
Jack Glass <jackadrianglass@gmail.com>
2020-11-19 20:09:54 -0700

    Updated Lobby Layout

--- batan-client/pages/lobby.vue
+++ batan-client/pages/lobby.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="lobby">
-    <h2>Lobby</h2>
-    <Chat />
-  </div>
+  <b-container class="lobby" fluid>
+    <b-row class="content" align-v="stretch">
+      <b-col class="games" cols="8">
+        <span class="selectors">
+          <b-button variant="success">Active Games</b-button>
+          <b-button variant="success">Create New Game</b-button>
+          <b-button variant="success">Join Game With Code</b-button>
+        </span>
+        <b-table striped hover :items="items">
+          <!--todo list all existing games-->
+        </b-table>
+      </b-col>
+      <b-col>
+        <Chat />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script lang="ts">
 export default {
   name: "Lobby",
-  components: {}
+  components: {},
+  data() {
+    return {
+      items: [
+        {Name: "Example's Game", Players: "3-4", Mode: "Normal"}
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="css" scoped>
+.lobby {
+  height: 90%
+}
+
+.content {
+  height: 100%;
+}
+
+.games {
+  margin: 0 8px 0 0;
+}
+
+.selectors {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-content: space-around;
+}
 </style>
