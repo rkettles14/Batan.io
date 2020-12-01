46931825c8c66d1a87240f7bf01284f492326920
rkettles14 <rkettles14@gmail.com>
2020-12-01 16:09:42 -0700

    Add toJsonString function for sending game object
    - add parseJson to client.
    -Minor formatting.

--- batan-client/store/games.js
+++ batan-client/store/games.js
@@ -1,21 +1,32 @@
 var game_mock = {
-  "game 1": {"id": "longUID", "board": {}},
-  "game 2": {"id": "longUID2", "board": {}}
+  'game 1': { id: 'longUID', board: {} },
+  'game 2': { id: 'longUID2', board: {} },
 }
 
 export const state = () => ({
   games: game_mock,
-  activeGame: "game 1"
+  activeGame: 'game 1',
 })
 
 export const mutations = {
   changeGame(state, new_active_game) {
-    if (Object.keys(state.games).includes(new_active_game)){
-      state.activeGame = new_active_game;
+    if (Object.keys(state.games).includes(new_active_game)) {
+      state.activeGame = new_active_game
     }
   },
   vuex_test(state, test_content) {
-    console.log('vuex test received');
-    console.log(test_content);
-  }
+    console.log('vuex test received')
+    console.log(test_content)
+  },
+  reviver(key, value) {
+    if (typeof value === 'object' && value !== null) {
+      if (value.dataType === 'Map') {
+        return new Map(value.value)
+      }
+    }
+    return value
+  },
+  parseJson(str) {
+    return JSON.parse(str, this.reviver)
+  },
 }
