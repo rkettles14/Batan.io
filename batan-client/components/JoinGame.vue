724295c242ed8ffe7544ae342e262a350b9df4ab
Jack Glass <jackadrianglass@gmail.com>
2020-12-01 17:30:54 -0700

    Add Join Game form to lobby

--- batan-client/components/JoinGame.vue
+++ batan-client/components/JoinGame.vue
@@ -1,7 +1,24 @@
 <template>
     <b-container fluid>
-        <!--todo create a form that takes in a game id to join it-->
-        <h1>Put the join game stuff here</h1>
+        <b-form>
+            <div class="form-element">
+                <label>Game Code:</label>
+                <b-form-input
+                    v-model="code"
+                    type="text"
+                    autocomplete="off"
+                    placeholder="Enter Game Code Here"
+                />
+            </div>
+            <div class="form-element">
+                <b-button
+                    type="submit"
+                    variant="success"
+                >
+                Join Game
+                </b-button>
+            </div>
+        </b-form>
     </b-container>
 </template>
 
@@ -9,9 +26,22 @@
 import Vue from 'vue'
 export default Vue.extend({
     name: "JoinGame",    
+    data() {
+        return {
+            code: "",
+        }
+    },
+
+    methods: {
+        join() {
+            //todo send request to server to join game
+        }
+    }
 });
 </script>
 
 <style scoped>
-
+.form-element {
+    margin: 2%;
+}
 </style>
