b487cc22334feb6e922612fdb3a9a139e747082b
Jack Glass <jackadrianglass@gmail.com>
2020-12-04 22:20:46 -0700

    Add join game event to the form

--- batan-client/components/JoinGame.vue
+++ batan-client/components/JoinGame.vue
@@ -1,7 +1,7 @@
 <template>
     <b-container fluid>
         <h2>Join Game</h2>
-        <b-form>
+        <b-form @submit.prevent="join">
             <div class="form-element">
                 <label>Game Code:</label>
                 <b-form-input
@@ -16,26 +16,36 @@
                     type="submit"
                     variant="success"
                 >
-                Join Game
+                <b-overlay :show="showOverlay" variant="success">
+                    Join Game
+                </b-overlay>
                 </b-button>
             </div>
         </b-form>
     </b-container>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
 export default Vue.extend({
     name: "JoinGame",    
     data() {
         return {
             code: "",
+            showOverlay: false,
         }
     },
 
     methods: {
         join() {
-            //todo send request to server to join game
+            if(this.$data.code === "") {
+                //do nothing
+                return;
+            }
+            this.$root.socket.emit("game/joinGame", {game_id: this.$data.code});
+            this.$root.socket.on("game/joined", (game) => {
+                window.$nuxt.$router.push("/game-screen");
+            });
         }
     }
 });
