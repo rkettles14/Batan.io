e119796d648f6c123e121c6f2723ceeb00023c1d
zacherysims <zach.md.sims97@gmail.com>
2020-12-05 18:01:12 -0700

    Got player actions going, just gotta get purchasing now

--- batan-client/pages/game-screen.vue
+++ batan-client/pages/game-screen.vue
@@ -12,8 +12,14 @@
       <b-col cols="5">
         <ResourceCards />
       </b-col>
-      <b-col cols="3">
+      <b-col cols="3" @click="displayPurchase()" v-if="showActions">
+        <PlayerActions/>
+      </b-col>
+      <b-col cols="3" @click="displayActions()" v-if="showDice">
         <Dice/>
+      </b-col>
+      <b-col cols="3" @click="displayDice()" v-if="showPurchase">
+        <h1>Purchase component goes here</h1>
       </b-col>
       <b-col cols="4">
         <GamePlayerStats />
@@ -28,6 +34,35 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "GameScreen",
+  data() {
+    return {
+      showDice: false,
+      showActions: true,
+      showPurchase: false,
+    }
+  },
+  methods: {
+
+    displayDice() {
+      this.showDice = true;
+      this.showActions = false;
+      this.showPurchase = false;
+    },
+
+    displayActions() {
+      setTimeout(() => {
+      this.showDice = false;
+      this.showActions = true;
+      this.showPurchase = false;
+      }, 4000);
+    },
+
+    displayPurchase() {
+      this.showDice = false;
+      this.showActions = false;
+      this.showPurchase = true;
+    },
+  }
 });
 
 </script>
