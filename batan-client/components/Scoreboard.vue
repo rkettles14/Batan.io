792ff952b9b2ea9d065a61ad3cd1d9f651127468
rkettles14 <rkettles14@gmail.com>
2020-12-12 18:41:26 -0700

    remove unused

--- batan-client/components/Scoreboard.vue
+++ batan-client/components/Scoreboard.vue
@@ -9,20 +9,3 @@
 
     </b-container>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    name: "Scoreboard",
-    computed: {
-        gameObject: function(){
-           return this.$store.state.games.active_games[this.$store.state.games.active_game.game_id].game_info;
-        },
-    }
-});
-</script>
-
-
-<style scoped>
-
-</style>
